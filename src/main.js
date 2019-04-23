9bdac6bf982778937584685e6d2df7574ae00bea
Jackie Chui <jackie2009hk@gmail.com>
2019-04-23 00:22:24 -0700

    Updated to use new showUI syntax

--- src/main.js
+++ src/main.js
@@ -1,36 +1,44 @@
 import App from './App.vue';
 
-figmaPlus.addCommand({ label: 'Find and Replace', action: () => {
-	figmaPlus.showUI({
-		title: 'Find and Replace',
-		callback: (tabContent1, tabContent2) => {
-			new figmaPlus.Vue({
-				el: tabContent1,
-				components: { App },
-				template: `<App :mode='mode'></App>`,
-				data: function() {
-					return {
-						mode: 'text'
-					};
+figmaPlus.addCommand({
+	label: 'Find and Replace',
+	action: () => {
+		figmaPlus.showUI({
+			title: 'Find and Replace',
+			tabs: [
+				{
+					title: 'Text',
+					vueComponent: figmaPlus.Vue.component('text', {
+						components: { App },
+						template: `<App :mode='mode'></App>`,
+						data: function() {
+							return {
+								mode: 'text'
+							};
+						}
+					})
+				},
+				{
+					title: 'Layer name',
+					vueComponent: figmaPlus.Vue.component('layer-name', {
+						components: { App },
+						template: `<App :mode='mode'></App>`,
+						data: function() {
+							return {
+								mode: 'layerName'
+							};
+						}
+					})
 				}
-			});
-			new figmaPlus.Vue({
-				el: tabContent2,
-				components: { App },
-				template: `<App :mode='mode'></App>`,
-				data: function() {
-					return {
-						mode: 'layerName'
-					};
-				}
-			});
-		},
-		width: 360,
-		positionX: 1,
-		positionY: 0.08,
-		tabs: ['Text', 'Layer Name']
-	});
-}, shortcut: {
-	mac: { command: true, key: 'F' },
-	windows: { control: true, key: 'F' }
-}, showInCanvasMenu: true });
+			],
+			width: 360,
+			positionX: 1,
+			positionY: 0.08
+		});
+	},
+	shortcut: {
+		mac: { command: true, key: 'F' },
+		windows: { control: true, key: 'F' }
+	},
+	showInCanvasMenu: true
+});
